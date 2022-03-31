81da3c6620325b25d63f9907b60e01d32910981a
JesseSinivuori <sinivuorii@gmail.com>
2022-03-31 14:44:56 +0300

    playercontroller added jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject ground;
     private Rigidbody playerRb;
     public float speed = 1000;
+    public bool isOnGround;
+    public float jumpForce = 1000;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-        
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            Jump();
+        }
+
+    }
+
+    private void Jump()
+    {
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isOnGround = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject)
+        {
+            isOnGround = true;
+        }
+
     }
 }
