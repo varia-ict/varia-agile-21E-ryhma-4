020a8238b9ded6ac2bc650ed8bc5f8b333685a80
karppis <skarppinen2000@gmail.com>
2022-04-14 13:23:53 +0300

    koristelin ensimmäistä maailmaa

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if(collisionGameObject.name == "Player")
+        {
+            Destroy(collisionGameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
