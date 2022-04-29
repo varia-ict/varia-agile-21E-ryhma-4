ccbfeaa33d1f8da22d68f21cb0846cfa2770248c
JesseSinivuori <sinivuorii@gmail.com>
2022-04-29 14:35:19 +0300

    animaatioita ja lyönti (broken)

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     public GameObject player;
+    private Enemy enemyScript;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
@@ -21,9 +24,18 @@ public class Attack : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
+            enemyScript.enemyHp -= 25;
+
+            if (enemyScript.enemyHp <= 0)
+            {
                 Destroy(other.gameObject);
+            }
+
         }
 
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerControllerScript.playerHp -= 25;
+        }
     }
-
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,26 +6,28 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject weapon;
     public GameObject enemy;
-    public GameObject ground;
     private Rigidbody playerRb;
     public GameObject playerSprite;
     public SpriteRenderer playerSpriteRenderer;
-    private BoxCollider playerBoxCollider;
+    public Animator playerAnimator;
+    public GameObject enemyWeapon;
+    public Enemy enemyScript;
     public float speed = 5;
     public bool isOnGround;
     public float jumpForce = 5;
     public bool gameOver;
     private float mapBottom = -5;
-    private float attackCoolDown = 0.2f;
+    private float attackCoolDown = 2f;
     public float playerHp = 100;
     private bool enemyCollision;
-    public bool facingRight;
+    public bool facingLeft;
+    private float jumpCoolDown = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerAnimator = playerAnimator.GetComponent<Animator>();
         playerRb = GetComponent<Rigidbody>();
-        playerBoxCollider = GetComponent<BoxCollider>();
         weapon.SetActive(false);
         playerSprite.transform.position = transform.position;
     }
@@ -36,13 +38,10 @@ public class PlayerController : MonoBehaviour
         if (playerHp <= 0)
         {
             gameOver = true;
+            playerAnimator.SetBool("dead", true);
         }
         if (!gameOver)
         {
-            //movement
-            float horizontalInput = Input.GetAxis("Horizontal");
-                transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-
             if (Input.GetKeyDown(KeyCode.Space) && isOnGround) //jump with space
             {
                 Jump();
@@ -51,31 +50,37 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Q)) // attack with q
             {
                 Attack();
+                
             }
 
             if (Input.GetKey(KeyCode.A)) //facing left
             {
                 playerSpriteRenderer.flipX = true;
-                facingRight = true;
-                
+                facingLeft = true;
+                playerAnimator.SetBool("run", true);
             }
 
 
             if (Input.GetKey(KeyCode.D)) //facing right
             {
                 playerSpriteRenderer.flipX = false;
-                facingRight = false;
-                
+                facingLeft = false;
+                playerAnimator.SetBool("run", true);
             }
 
-            if(!facingRight)
+            if(!facingLeft)
             {
                 playerSprite.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             }
 
-            else
+            else if (facingLeft)
             {
                 playerSprite.transform.position = new Vector3(transform.position.x -0.22f, transform.position.y, transform.position.z);
+            }
+
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && !Input.GetKeyDown(KeyCode.Q) && !Input.GetKeyDown(KeyCode.Space) && isOnGround)
+            {
+                playerAnimator.SetBool("run", false);
             }
         }
 
@@ -87,16 +92,32 @@ public class PlayerController : MonoBehaviour
 
         if (enemyCollision)
         {
-            playerHp -= 25;
+            playerHp -= 0;
         }
+
 
 
     }
 
+    private void FixedUpdate()
+    {
+        if (!gameOver)
+        {
+            //movement
+            float horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * horizontalInput * speed * Time.fixedDeltaTime);
+        }
+    }
     private void Jump() //jump
     {
         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); //add force to jump
         isOnGround = false;
+        playerAnimator.SetBool("isOnGround", false);
+        playerAnimator.SetTrigger("jump");
+
+        
+
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -104,11 +125,14 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject) //check if on ground
         {
             isOnGround = true;
+            playerAnimator.SetBool("isOnGround", true);
+            StartCoroutine(JumpCoolDown(jumpCoolDown));
         }
 
         if (collision.gameObject.CompareTag("Enemy")) //check if collided with enemy
         {
             enemyCollision = true;
+            playerAnimator.SetTrigger("enemyCollision");
         }
 
     }
@@ -117,13 +141,32 @@ public class PlayerController : MonoBehaviour
     {
         
         yield return new WaitForSeconds(attackCoolDown);
-        weapon.SetActive(false);
+
+        
+        
+
+
     }
 
     private void Attack() //attack
     {
+        if (facingLeft)
+        {
+            weapon.transform.position = new Vector3(transform.position.x - 0.33f, transform.position.y - 0.1f, transform.position.z);
+        }
+
+        else if (!facingLeft)
+        {
+            weapon.transform.position = new Vector3(transform.position.x + 0.11f, transform.position.y - 0.1f, transform.position.z);
+        }
         weapon.SetActive(true);
-        StartCoroutine(AttackCoolDown(attackCoolDown));
+        playerAnimator.SetTrigger("attack");
+        weapon.SetActive(false);
+    }
+
+    IEnumerator JumpCoolDown(float jumpCoolDown) //attack cooldown
+    {
+        yield return new WaitForSeconds(jumpCoolDown);
     }
 
 }
