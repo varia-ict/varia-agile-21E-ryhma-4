a4c6fc9388ace5ce1fc918cbfa6d0924df9a3531
Safincool <safin.legend@gmail.com>
2022-04-29 10:55:53 +0300

    Tein game over screen

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+ public class GameOverScreen : MonoBehaviour
+{
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("World");
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("Menu"); 
+    }
+}
