a2af328bf0c0b9ea666dc8acff7df3d7ef540dc4
casperiii <kasper.orn@gmail.com>
2022-05-12 10:03:52 +0300

    scriptin lisäys

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public bool mustPatrol;
+    public float walkSpeed;
+    public Transform groundCheckPos;
+    public LayerMask groundLayer;
+    public Collider2D bodyCollider;
+    private bool mustTurn;
+
+    public Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mustPatrol = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mustPatrol)
+        {
+            Patrol();
+         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (mustPatrol)
+        {
+            mustTurn = !Physics2D.OverlapCircle(groundCheckPos.position, 0.1f, groundLayer);
+        }
+    }
+
+    void Patrol()
+    {
+        if (mustTurn || bodyCollider.IsTouchingLayers(groundLayer))
+        {
+            Flip();
+        }
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+
+        mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustPatrol = true;
+    }
+}
